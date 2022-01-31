61a845387bb1f5eba0c3ec9d8dabc39a262733da
Ken Ojiri <kojiri@pivotal.io>
2022-01-31 18:43:31 +0900

    fix common_ubuntu_release_upgrade() in jbox-common.sh

--- docs/jbox-common.sh
+++ docs/jbox-common.sh
@@ -196,16 +196,18 @@ common_ubuntu_release_upgrade() {
 
     sudo apt-get update
     sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
-    sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -f -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
     sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
-    sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes -o Dpkg::Options::=\"--force-confnew\" update-manager-core"
-    sudo do-release-upgrade -c
     sudo do-release-upgrade -f DistUpgradeViewNonInteractive
     sudo reboot
   fi
 
   # 18.04 (bionic)
   if [ $DISTRIB_CODENAME = "bionic" ]; then
+    if [ $(uname -r | awk -F- '{print $1}') != "4.15.0" ]; then
+      sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
+      sudo reboot
+    fi
+
     ### add sources.list ###
     if [ -f /etc/apt/sources.list.d/VMW-internal-mirror-xenial.list ]; then
       sudo rm -f /etc/apt/sources.list.d/VMW-internal-mirror-xenial.list
@@ -216,13 +218,17 @@ common_ubuntu_release_upgrade() {
     sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
     sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
     sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes -o Dpkg::Options::=\"--force-confnew\" update-manager-core"
-    sudo do-release-upgrade -c
     sudo do-release-upgrade -f DistUpgradeViewNonInteractive
     sudo reboot
   fi
 
   # 20.04 (focal)
   if [ $DISTRIB_CODENAME = "focal" ]; then
+    if [ $(uname -r | awk -F- '{print $1}') != "5.4.0" ]; then
+      sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y --force-yes -o Dpkg::Options::=\"--force-confnew\""
+      sudo reboot
+    fi
+
     ### add sources.list ###
     if [ -f /etc/apt/sources.list.d/VMW-internal-mirror-xenial.list ]; then
       sudo rm -f /etc/apt/sources.list.d/VMW-internal-mirror-xenial.list
