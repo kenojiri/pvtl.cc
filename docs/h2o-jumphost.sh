f8407b0df0c7d4753ae7bef27624b4accbcba759
Ken Ojiri <kojiri@vmware.com>
2023-09-22 07:57:14 +0000

    fix h2o-jumphost.sh

--- docs/h2o-jumphost.sh
+++ docs/h2o-jumphost.sh
@@ -5,9 +5,9 @@ if test -z "$BASH_VERSION"; then
   exit 1
 fi
 
-if [ -z "$GITHUB_AUTH_CREDS" -o -z "$NEW_HOSTNAME" ] ; then
+if [ -z "$GITHUB_AUTH_CREDS" ] ; then
   echo "Please run this script with GITHUB_AUTH_CREDS and NEW_HOSTNAME," >&2
-  echo "such as 'curl -skL https://pvtl.cc/nimbus-k3s.sh | GITHUB_AUTH_CREDS=**** NEW_HOSTNAME=nimbus?? bash'" >&2
+  echo "such as 'curl -skL https://pvtl.cc/h2o-jumphost.sh | GITHUB_AUTH_CREDS=**** bash'" >&2
   exit 1
 else
   CURL="curl -uk ${GITHUB_AUTH_CREDS}"
